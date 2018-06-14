01045d0c5b2ff49a6c128b5f7af4a54b55071656
nazareh <nazarehturmina@gmail.com>
2018-06-14 16:33:05 +0800

    Add more entities:     Recipe 1 - N Ingredient     Ingredient 1 - 1 UnitOfMeasure

--- src/main/java/turmina/nazareh/spring5recipeapp/domain/Recipe.java
+++ src/main/java/turmina/nazareh/spring5recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package turmina.nazareh.spring5recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -26,6 +27,8 @@ public class Recipe {
     @OneToOne (cascade = CascadeType.ALL)
     private Notes notes;
 
+    @OneToMany (cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     public Long getId() {
         return id;
@@ -105,5 +108,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }

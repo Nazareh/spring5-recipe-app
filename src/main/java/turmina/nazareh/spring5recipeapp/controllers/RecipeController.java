3284d926e6d16a4f764a944c76441c07ccda59c2
Nazareh <nazarehturmina@gmail.com>
2019-03-31 20:00:51 +0800

    Adding Create Recipe

--- src/main/java/turmina/nazareh/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/turmina/nazareh/spring5recipeapp/controllers/RecipeController.java
@@ -5,8 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import turmina.nazareh.spring5recipeapp.commands.RecipeCommand;
+import turmina.nazareh.spring5recipeapp.domain.Recipe;
 import turmina.nazareh.spring5recipeapp.services.RecipeService;
 
 @Slf4j
@@ -25,5 +29,19 @@ public class RecipeController {
         log.debug("Getting recipe page.");
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe",new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand ){
+        RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + savedRecipeCommand.getId();
     }
 }

7c03b24040d9f56813c40fb1589ce43b7b1f6245
Nazareh <nazareh.turmina@tieto.com>
2019-08-09 16:54:34 +0800

    Persisting images to Database

--- src/main/java/turmina/nazareh/spring5recipeapp/services/ImageServiceImpl.java
+++ src/main/java/turmina/nazareh/spring5recipeapp/services/ImageServiceImpl.java
@@ -2,15 +2,47 @@ package turmina.nazareh.spring5recipeapp.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import turmina.nazareh.spring5recipeapp.domain.Recipe;
+import turmina.nazareh.spring5recipeapp.repositories.RecipeRepository;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+    private RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
 
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
-        log.debug("Received a file");
+
+        Recipe recipe = recipeRepository.findById(recipeId).get();
+
+        try {
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()){
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+
+        } catch (IOException e) {
+            // todo handle better
+            log.debug("Error occured",e);
+            e.printStackTrace();
+        }
+
 
     }
 }

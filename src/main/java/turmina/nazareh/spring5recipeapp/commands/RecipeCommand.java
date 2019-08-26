a9e72895fcbecce1d451067efce3fdb6a79b38de
nazareh <nazarehturmina@gmail.com>
2019-08-26 11:12:15 +0800

    Data Validation with Spring MVC

--- src/main/java/turmina/nazareh/spring5recipeapp/commands/RecipeCommand.java
+++ src/main/java/turmina/nazareh/spring5recipeapp/commands/RecipeCommand.java
@@ -3,8 +3,13 @@ package turmina.nazareh.spring5recipeapp.commands;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 import turmina.nazareh.spring5recipeapp.domain.Difficulty;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,16 +18,30 @@ import java.util.Set;
 @NoArgsConstructor
 public class RecipeCommand {
     private Long id;
-    private String description;
-    private Set<CategoryCommand> categories = new HashSet<>();
+    @NotBlank
+    @Size(min = 3, max = 255)private String description;
+
+    @Min(1)
+    @Max(999)
     private Integer cookTime;
-    private Difficulty difficulty;
+
+    @NotBlank
     private String directions;
-    private Set<IngredientCommand> ingredients = new HashSet<>();
-    private NotesCommand notes;
+
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+
+    @Min(1)
+    @Max(999)
     private Integer servings;
-    private String source;
+
+    @URL
     private String url;
     private Byte[] image;
+    private String source;
+    private Set<IngredientCommand> ingredients = new HashSet<>();
+    private NotesCommand notes;
+    private Difficulty difficulty;
+    private Set<CategoryCommand> categories = new HashSet<>();
 }
--- src/main/java/turmina/nazareh/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/turmina/nazareh/spring5recipeapp/controllers/RecipeController.java
@@ -2,19 +2,20 @@ package turmina.nazareh.spring5recipeapp.controllers;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 import turmina.nazareh.spring5recipeapp.commands.RecipeCommand;
-import turmina.nazareh.spring5recipeapp.exceptions.NotFoundException;
 import turmina.nazareh.spring5recipeapp.services.RecipeService;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
 public class RecipeController {
 
+    private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
     private RecipeService recipeService;
 
     public RecipeController(RecipeService recipeService) {
@@ -30,20 +31,25 @@ public class RecipeController {
     @GetMapping("recipe/new")
     public String newRecipe(Model model){
         model.addAttribute("recipe",new RecipeCommand());
-        return "recipe/recipeform";
+        return RECIPE_RECIPEFORM_URL;
     }
 
     @GetMapping("/recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-        return "recipe/recipeform";
+        return RECIPE_RECIPEFORM_URL;
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand,Model model ){
-        RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand recipeCommand, BindingResult bindingResult){
 
-        return "redirect:/recipe/" + savedRecipeCommand.getId() +"/show";
+        if(bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().forEach(objectError -> log.debug(objectError.toString()));
+
+            return RECIPE_RECIPEFORM_URL;
+        }
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 
     @GetMapping("recipe/{id}/delete")
